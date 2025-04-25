663db7ff61f3b1c39c3ad40d6cbb6ec79535d79b
Eden Yemini <eden881@gmail.com>
2025-04-26 02:01:16 +0300

    Added nostalgic handheld game

---
+++ 038-nostalgic-handheld-game/README.md
@@ -0,0 +1,7 @@
+# nostalgic-handheld-game-0.lua
+
+| Code size | Cycles / voxel |
+| --------- | -------------- |
+| 116       | 21.00          |
+
+![](nostalgic-handheld-game-0.png)
---
+++ 038-nostalgic-handheld-game/nostalgic-handheld-game-0.lua
@@ -0,0 +1,13 @@
+if inrange(z, 0, 1) and abs(x) < 3 and inrange(y, 1, 5) then
+	return z == 0 and GREEN
+elseif z == 1 and abs(x) == 3 and inrange(y, 1, 5) then
+	return GREY
+elseif abs(z) < 2 and abs(x) < 5 then
+	return x - y < 9
+elseif z == 2 then
+	if (x == 1 and y == -3) or (x == 3 and y == -2) then
+		return RED
+	elseif min(abs(x + 2) + abs(y + 2)) < 2 then
+		return BLACK
+	end
+end
