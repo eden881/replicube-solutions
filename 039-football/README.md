c7a09de6e1fcdd3ca0977fc93c95d4b9de03d5f4
Eden Yemini <eden881@gmail.com>
2025-04-26 05:33:07 +0300

    Added football

---
+++ 039-football/README.md
@@ -0,0 +1,7 @@
+# football-0.lua
+
+| Code size | Cycles / voxel |
+| --------- | -------------- |
+| 42        | 9.28           |
+
+![](football-0.png)
---
+++ 039-football/football-0.lua
@@ -0,0 +1,6 @@
+if abs(y) < 3 and abs(z) < 3 then
+	if y == 2 and abs(z) < 2 and abs(x) < 3 and x % 2 == 0 then
+		return WHITE
+	end
+	return BROWN
+end
